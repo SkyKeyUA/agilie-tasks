a8e1fa3a32d43195b74dea79382168f6851da57d
SkyKeyUA <veremiienkoov96@gmail.com>
2023-04-13 15:15:00 +0300

    task 1 (numbers game) done

--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -10,11 +10,49 @@ export const Game = () => {
   const onClickCompare = () => {
     let n1 = Number(numberOne);
     let n2 = Number(numberTwo);
-    while (n1 !== n2) {}
+    if (n1 >= 0 && n2 >= 0) {
+      if (n1 === n2) {
+        return setResult(`We don't need to convert (${n1} = ${n2})`);
+      }
+      if (n1 * 2 === n2) {
+        return setResult(
+          `We can convert the Number 1 to the Number 2 by multiplying this number by 2 (${n1} * 2 = ${n2}) | ${
+            n1 * 2
+          } = ${n2}`,
+        );
+      }
+      if (n2 * 2 === n1) {
+        return setResult(
+          `We can convert the Number 2 to the Number 1 by multiplying this number by 2 (${n2} * 2 = ${n1}) | ${
+            n2 * 2
+          } = ${n1}`,
+        );
+      }
+      if (Number(n1 + '1') === n2) {
+        return setResult(
+          `We can convert the Number 1 to the Number 2 by adding one to the right (${n1} + 1 = ${n2}) | ${
+            n1 + '1'
+          } = ${n2}`,
+        );
+      }
+      if (Number(n2 + '1') === n1) {
+        return setResult(
+          `We can convert the Number 2 to the Number 1 by adding one to the right (${n2} + 1 = ${n1}) | ${
+            n2 + '1'
+          } = ${n1}`,
+        );
+      }
+      if (n1 !== n2) {
+        return setResult(`We cannot convert one number to another`);
+      }
+    } else {
+      return setResult('Please, selects positive numbers');
+    }
   };
   return (
     <div className={styles.game}>
-      <div className={styles.title}>Numbers game</div>
+      <div className={styles.title}>Task 1</div>
+      <div className={styles.info}>Numbers game</div>
       <div className={styles.info}>
         <div className={styles.user}>Number: 1</div>
         <input
@@ -33,10 +71,12 @@ export const Game = () => {
           className={styles.number}
         />
       </div>
-      <button className={styles.btn}>Compare</button>
+      <button onClick={onClickCompare} className={styles.btn}>
+        Compare
+      </button>
       <div className={styles.info}>
         <div className={styles.user}>Result:</div>
-        <div className={styles.result}></div>
+        <div className={styles.result}>{result}</div>
       </div>
     </div>
   );
